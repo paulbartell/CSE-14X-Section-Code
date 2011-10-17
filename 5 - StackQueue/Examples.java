b2573a13c3d3a2e4e9b43a266dce6427a533d328
David Mah <mahhaha@gmail.com>
2011-10-16 17:21:17 -0700

    code for stack problems

--- 5 - StackQueue/Examples.java
+++ 5 - StackQueue/Examples.java
@@ -1,16 +1,60 @@
 import java.util.*;
 public class Examples {
-  public static void main(String[] args) {
-    Stack<Integer> s = new Stack<Integer>();
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    System.out.println(s);
-  }
-  public static void stutter(Stack<Integer> st) {
-    return;
+  public static void stutter(Stack<Integer> s) {
+    Queue<Integer> q = new LinkedList<Integer>();
+
+    while(!s.isEmpty()) {
+      int temp = s.pop();
+      q.add(temp);
+      q.add(temp);
+    }
+    queueToStack(q, s);
+
+    stackToQueue(s, q);
+    queueToStack(q, s);
   }
   public static boolean equals(Stack<Integer> s1, Stack<Integer> s2) {
-    return false;
+    Stack<Integer> s3 = new Stack<Integer>();
+    // check size and return maybe immediately
+    if(s1.size() != s2.size()) {
+      return false;
+    }
+    while(!s1.isEmpty()) {
+      int a1 = s1.pop();
+      int a2 = s2.pop();
+      s3.push(a1);
+      s3.push(a2);
+
+      if(a1 != a2) {
+      // if no match, move everything back and then return
+        while(!s3.isEmpty()) {
+          int b2 = s3.pop();
+          int b1 = s3.pop();
+          s1.push(b1);
+          s2.push(b2);
+        }
+      return false;
+      }
+    }
+    while(!s3.isEmpty()) {
+      int b2 = s3.pop();
+      int b1 = s3.pop();
+      s1.push(b1);
+      s2.push(b2);
+    }
+    return true;
+
+  }
+
+  public static void queueToStack(Queue<Integer> q, Stack<Integer> s) {
+    while(!q.isEmpty()) {
+      s.push(q.remove());
+    }
+  }
+
+  public static void stackToQueue(Stack<Integer> s, Queue<Integer> q) {
+    while(!s.isEmpty()) {
+      q.add(s.pop());
+    }
   }
 }
