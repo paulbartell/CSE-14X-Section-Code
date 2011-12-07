12de3f59692383a0d7af5e64f66b90a29b5f7dfa
David Mah <mahhaha@gmail.com>
2011-12-07 13:03:14 -0500

    Mad annotations to linkedlist code

--- 18 - LinkedListReview/Examples.java
+++ 18 - LinkedListReview/Examples.java
@@ -12,31 +12,57 @@ public int evenSum() {
   return sum;
 }
 
+// Steps
+// Before and After diagram
+// Choose a perspective -- how many nodes are relevant?
+// Make a perspective diagram with before/after arrows
+//
+// Choose temporary variables for nodes in the perspective
+// Document the reassignments for nodes in the perspective
+// Be confident in these
+//
+// Find out where you would need to place a current pointer in order to set up your temp variables
+// Find out how to assign the variables for the perspective case
+// Find out how to assign the variables for the front case
+//
+// Find a condition where you either cannot set up current or you cannot set up your temp variables
+// This will be the condition of your traversal loop
+//
+// Then code
+
 
 public void switchPairs() {
-  // Should I not do anything at all
+  // What situations should I not do anything at all
   if(front == null || front.next == null)
     return;
+  // Handle the front case
   ListNode f = front;
   ListNode s = front.next;
   ListNode r = front.next.next;
-  //p is actually front
+  // P is analogous to front for the first case
   front  = s;
   f.next = r;
   s.next = f;
 
-  //the rest
+  // The rest
+  // Place current in its necessary spot for the first perspective
   ListNode current = front.next;
+
+  // In what situations can we guarantee that the variables can get set up?
   while(current.next != null && current.next.next != null) {
+    // Assign temps
     ListNode p = current;
     f = p.next;
     s = f.next;
     r = s.next;
 
+    // Rearrange
     p.next = s;
     f.next = r;
     s.next = f;
 
+    // How can we get current from it's position for this perspective
+    // to the new perspective for the next perspective
     current = current.next.next;
   }
 }
--- 18 - LinkedListReview/SwitchPairs.java
+++ 18 - LinkedListReview/SwitchPairs.java
@@ -1,17 +1,43 @@
+// Steps
+// Before/After Diagram
+// Choose perspective
+// Don't separate the front case, front/rest are handled with the same code
+// Assign Temporary Variables
+// Figure out rearranging. Attaching to the parent is through return
+//
+// When is the linked list not large enough for our perspective? That will be the Base Case
+
 public void switchPairs() {
   front = switchPairs(front);
 }
 
 public ListNode switchPairs(ListNode node) {
+  // When the linked list is not large enough for our perspective
   if(node == null || node.next == null)
     return node;
+  // Assign temporary variables to the perspective
   ListNode first  = node;
   ListNode second = node.next;
   ListNode rest   = second.next;
 
   // Fix the rest of the list, and attach it where it needs to go
-  first.next = switchPairs(rest);
+  first.next = switchPairs(rest); // f.next = r AND handles the rest of the list
+
   // Fix the nodes in my perspective here
-  second.next = first;
-  return second;
+  second.next = first; // Equivalent in other example to: s.next = f
+
+  // To attach to the parent, we return
+  return second; // Equivalent to in other example to: p.next = s
 }
+
+// When do we NOT use recursion
+//
+// You have to return a brand new linked list
+//   the problem will read something like --
+//   extract every even node or so into a second linked list and return that ). 
+//   You won't be able to solve such a problem without being able to return multiple items.
+//
+// Rearrangements are difficult
+//   Assuming that the rest of the list beyond the perspective is in solved order
+//   What extra details do we have to handle to combine the perspective with the rest?
+//   This can be really hard if.. you have to inject a perspective node into the middle of the rest
